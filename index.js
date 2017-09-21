c34c18c52ca626637e1b4f1181c47bd12bda373f
Luiz Irber <luiz.irber@gmail.com>
2017-09-21 13:50:55 -0700

    Perform basic validation on FASTQStream

--- index.js
+++ index.js
@@ -17,6 +17,28 @@ class FASTQStream extends Transform {
     this.block = []
   }
 
+  basicValidation (record) {
+    var valid = true
+    if (record['seq'].length !== record['qual'].length) {
+      this.emit('error', 'sequence and quality length should be the same in record ' + record['name'])
+      valid = false
+    }
+    if (record['name'].slice(0, 1) !== '@') {
+      this.emit('error', 'sequence header must start with "@" in record ' + record['name'])
+      valid = false
+    }
+    if (record['name2'].slice(0, 1) !== '+') {
+      this.emit('error', 'sequence header must start with "@" in record ' + record['name'])
+      valid = false
+    }
+    if (record['name'].slice(1) !== record['name2'].slice(1) &&
+        record['name2'].slice(1).length !== 0) {
+      this.emit('error', 'name2 must be empty or match name in record ' + record['name'])
+      valid = false
+    }
+    return valid
+  }
+
   _processFASTQ (last) {
     var lines = this._rawbuf.split(/\r?\n/)
     var i = 0
@@ -24,12 +46,15 @@ class FASTQStream extends Transform {
     for (i = 0; i < lines.length - 1; i++) {
       this.block.push(lines[i])
       if (this.block.length === 4) {
-        this.push({
+        var record = {
           name: this.block[0],
           seq: this.block[1],
           name2: this.block[2],
           qual: this.block[3]
-        })
+        }
+        if (this.basicValidation(record)) {
+          this.push(record)
+        }
         this.block = []
       }
     }
--- test/fastqstream.js
+++ test/fastqstream.js
@@ -28,7 +28,7 @@ test('FASTQStream: basic validation', function (t) {
 
   t.plan(2)
   var records = []
-  reader.pipe(new FASTQStream({validate: true}))
+  reader.pipe(new FASTQStream())
         .on('data', function (data) {
           t.equal(data['seq'].length, data['qual'].length,
                   'seq and qual have the same length')
@@ -42,11 +42,32 @@ test('FASTQStream: basic validation', function (t) {
         })
 })
 
-test('FASTQStream: validation error', function (t) {
+test('FASTQStream: basic validation in file with errors', function (t) {
   var reader = fs.createReadStream('./test/test-data/wrong_length.fq')
 
   t.plan(1)
   reader.pipe(new FASTQStream())
+        .on('data', function () {})
+        .on('error', function (err) {
+          if (err) {
+            t.pass(err)
+          }
+        })
+        .on('end', function () {
+          t.fail('Invalid file, should throw an error')
+        })
+})
+
+test('FASTQValidator: validation error', function (t) {
+  var reader = fs.createReadStream('./test/test-data/wrong_length.fq')
+
+  t.plan(1)
+  reader.pipe(new FASTQStream())
+        .on('error', function (err) {
+          if (err) {
+            t.pass(err)
+          }
+        })
         .pipe(new FASTQValidator())
         .on('error', function (err) {
           if (err) {
